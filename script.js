9969bce924bf65ad812a1abd3bd189842892cb52
xMing <526817087@qq.com>
2020-03-02 15:12:19 +0800

    xiao bug

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ function focusinput(ele){
 	text = ele.value;
 	if(text == "输入你的内容") {
 		ele.value = "";
+		ele.style.color = "#000";
 		document.getElementById("clear").innerHTML = "↓吃掉↓";
 	}else if(text == "") {
 		ele.style.color = "rgba(0,0,0,0.5)";
 		ele.value = "输入你的内容";
 		document.getElementById("clear").innerHTML = ">_<没有吃的";
-	}else{
-		ele.style.color = "#000";
 	}
 }
